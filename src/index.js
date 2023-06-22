1de05082acec3dd2711e2f679e4508f4efa353f8
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-22 18:44:09 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Welcome to my website</h1>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Pellentesque ac volutpat mi, id eleifend turpis.
+In euismod tortor sed eleifend semper.
+Fusce in lorem et nunc hendrerit congue vel vel lectus.
+Curabitur lobortis, lorem et imperdiet fermentum, mauris turpis rutrum risus.
+  
+</p>
+`;
